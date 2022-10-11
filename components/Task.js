2af972cce262e3e799a525630b4f054666592896
Luyen Ngo <94201658+luyenngongo2002@users.noreply.github.com>
2022-10-11 14:49:43 +0700

    Count number

--- components/Task.js
+++ components/Task.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const Task = (props) => {
     return (
@@ -13,37 +13,5 @@ const Task = (props) => {
     );
 };
 
-const styles = StyleSheet.create({
-    item: {
-        backgroundColor: "#FFF",
-        padding: 15,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        borderRadius: 15,
-        marginBottom: 20,
-    },
-    square: {
-        width: 24,
-        height: 24,
-        backgroundColor: "#55BCF6",
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
-    },
-    itemLeft: {
-        flexDirection: "row",
-    },
-    itemText: {
-        maxWidth: "80%",
-    },
-    circle: {
-        height: 12,
-        width: 14,
-        backgroundColor: "#55BCF6",
-        borderRadius: 5,
-        borderWidth: 2,
-    },
-});
 
 export default Task;
